2384ae19901acc08e96a1b3c3d24a209097678c5
Komsomol <farhad.agza@gmail.com>
2019-09-01 20:55:02 +0100

    initial files

---
+++ takepic.js
@@ -0,0 +1,29 @@
+const spawn = require('child_process').spawn;
+const btoa = require('btoa');
+
+exports.raspistill = function (args, callback) {
+    // flags must be passed as an array to spawn
+    if (typeof args == 'string') {
+        args = args.split(' ');
+    }
+    console.log(args);
+    var raspistill = spawn('raspistill', args);
+
+    var arr = [];
+
+    raspistill.stdout.on('data', function (data) {
+        // console.log(data)
+        arr.push(data);
+    });
+
+    raspistill.stdout.on('close', function (code) {
+        var buffer = new Buffer.concat(arr);
+        var base64 = buffer.toString('base64');
+        var image = `data:image/jpeg;base64,${base64}`;
+        callback(image);
+    });
+
+    raspistill.stdout.on('error', function (err) {
+        throw err;
+    });
+};
